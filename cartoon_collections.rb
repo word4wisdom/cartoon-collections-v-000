bae7b2f7ce45bade7309eccde33b038fc10a7b0e
Joan' Araujo <wisdombthegame@gmail.com>
2020-09-08 05:05:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do | name, index | 
     puts "#{index + 1}. #{name}"
+  end
+end 
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |elements|
